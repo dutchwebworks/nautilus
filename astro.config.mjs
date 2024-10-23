874ec3bea3dbe4bcf6a60b5d909e2b6a57439766
Dennis Burger <dennis@dutchwebworks.nl>
2024-10-23 08:49:17 +0200

    Initial files of Nautlius

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://nautilus.dutchwebworks.nl',
+    compressHTML: false,
+    trailingSlash: 'never',
+});
---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Nautilus » ";
+export const WEBSHOP_NAME = "Nautilus";
+export const AUTHOR = "Dennis Burger";
