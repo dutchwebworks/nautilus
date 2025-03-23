cc641e50c491fe852b3b1dd6a2aa3db0c8478a41
Dennis Burger <dennis@dutchwebworks.nl>
2025-03-23 11:33:42 +0100

    Installed Prettier. Ran Prettier on all files.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://nautilus.dutchwebworks.nl',
+    site: "https://nautilus.dutchwebworks.nl",
     compressHTML: false,
-    trailingSlash: 'never',
+    trailingSlash: "never",
 });
